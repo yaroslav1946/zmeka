ad624bca2ccd2ef5984c393e29cb1fed73dd7508
Denis <Denis@test.ru>
2023-02-03 19:23:06 +0300

    добавили класс Point, хранит координаты точки

---
+++ Point.h
@@ -0,0 +1,9 @@
+#pragma once
+struct Point
+{
+	int x, y;
+	Point():x(-1), y(-1) {}
+	Point(int a, int b) : x(a), y(b) {}
+	bool operator==(const Point& other) { return x == other.x && y == other.y; }
+};
+
